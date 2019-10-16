168517a4af02bf6d65a9256cb72cfdbe7ba1608f
hchoi84@gmail.comlszYeuzF3EErZcYc6exit <hchoi84@gmail.com>
2019-10-16 11:18:39 -0700

    DTOs for Client Side, AutoMapper

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -44,7 +44,7 @@ namespace DatingApp.API.Data
     public async Task<User> Register(User user, string password)
     {
       byte[] passwordHash, passwordSalt;
-      createPasswordHash(password, out passwordHash, out passwordSalt);
+      CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
       user.PasswordHash = passwordHash;
       user.PasswordSalt = passwordSalt;
@@ -55,7 +55,7 @@ namespace DatingApp.API.Data
       return user;
     }
 
-    private void createPasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
       using (var hmac = new System.Security.Cryptography.HMACSHA512())
       {
